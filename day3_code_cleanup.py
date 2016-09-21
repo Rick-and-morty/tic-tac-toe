a3bd6d8d890174b3d71b39ad6e3716dd4271c360
Rick-and-morty <te5840@gmail.com>
2016-09-21 10:53:02 -0400

    fun game

--- day3_code_cleanup.py
+++ day3_code_cleanup.py
@@ -1,6 +1,13 @@
 # inventory tracker RPG
 
-player = {}
+player = {"inventory": {"red potion": {'quantity': 20}}}
+
+{
+        "red potion": {
+              "quantity": 1
+         }
+}
+
 player_name = input("welcome traveler! what is your name? ")
 player["name"] = player_name
 print(player)
@@ -8,6 +15,11 @@ while True:
     choice = input("would you like to (i)nspect your inventory, or (a)dd to your inventory?\n>")
 
     if choice == "a":
-        print("adding item to inventory")
+        item_name = input('what is the item name? ')
+        item_quantity = int(input("how many? "))
+        player["inventory"][item_name] = {"quantity": item_quantity}
     elif choice == 'i':
         print("looking at inventory")
+        for item in player["inventory"].keys():
+            quantity = player["inventory"][item]["quantity"]
+            print("name{} - {}".format(item, quantity))
