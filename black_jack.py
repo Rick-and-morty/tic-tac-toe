d187ce4cfb421573c18914dfd6d130d8413aaad2
Rick-and-morty <te5840@gmail.com>
2016-09-26 08:57:06 -0400

    still trying

--- black_jack.py
+++ black_jack.py
@@ -6,7 +6,16 @@ class Table:
     def __init__(self, player, dealer):
         self.player = player
         self.dealer = dealer
-        pass
+
+
+class BJT:
+
+    def __init__(self):
+        self.hand = []
+
+'''    def hit(self, )'''
+
+# hit will add a card to the hand from the deck
 
 
 class Cards:
@@ -21,16 +30,16 @@ class Deck:
 
     def __init__(self):
         self.card_index = []
-        self.suit = ["Hearts"], ["Spades"], ["Clubs"], ["Diamonds"]
-        self.card_value = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-                           "7": 7, "8": 8, "9": 9, "10": 10, "J": 10,
-                           "Q": 10, "K": 10, "A": 11}
-
-        def mass(self):
-            pass
-
-# computer = Table("dealer")
-# human = Table("player")
+        self.suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+        self.card_values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+                            "7": 7, "8": 8, "9": 9, "10": 10, "J": 10,
+                            "Q": 10, "K": 10, "A": 11}
+        for suit in self.suits:
+            for card_value in self.card_values:
+                self.card_index.append((suit, card_value))
 
 
-print(card_value)
+deck = Deck()
+shuffle(deck.card_index)
+
+print(deck.card_index)
